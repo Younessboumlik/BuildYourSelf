c6dbb94ce161ec80e6c435c0b4d56167033639b6
YounessBoumlik <younessboumlik1949@gmail.com>
2024-06-04 02:08:26 +0100

    update

--- resources/views/creataccount.blade.php
+++ resources/views/creataccount.blade.php
@@ -3,62 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <title>Create Account</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body >
-  @if ($creatcheck)
-  <div class="toast align-items-center bg-danger-subtle border-0 mx-auto w-100" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="d-flex">
-      <div class="toast-body text-danger">
-       erruer : se compte est deja existe.
-      </div>
-      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-  </div>
-@endif
-    <div class="card mb-3 w-75 mx-auto border-0" style="margin-top: 10%;">
-        <div class="row g-0 ">
-          <div class="col-md-4 d-flex justify-content-center">
-            <img src="/5fcddbf4f1ce4bf6a2492d5c3bec559d.png" class="img-fluid rounded-start align-self-center">
-          </div>
-          <div class="col-md-8 bg-light-subtle  shadow-lg rounded-3">
-            <form class="card-body" action ="/creataccount/new" method="post">
-            @csrf
-              <h1 class="card-title text-center ">log in</h1>
-              <div class="input-group mb-3 mt-5">
-                <span class="input-group-text" id="basic-addon1">nom</span>
-                <input type="text" class="form-control" placeholder="nom" aria-label="Username" aria-describedby="basic-addon1" name = "firstname" required>
-              </div>
-              <div class="input-group mb-3 mt-5">
-                <span class="input-group-text" id="basic-addon1">prenom</span>
-                <input type="text" class="form-control" placeholder="prenom" aria-label="Username" aria-describedby="basic-addon1" name = "lastname" required>
-              </div>
-              <div class="input-group mb-3 mt-5">
-                <span class="input-group-text" id="basic-addon1">email</span>
-                <input type="email" class="form-control" placeholder="email" aria-label="Username" aria-describedby="basic-addon1" name = "email" required>
-              </div>
-              <div class="input-group mb-3 mt-5">
-                <span class="input-group-text" id="basic-addon1">password</span>
-                <input type="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon1" name = "password" required>
-              </div>
-              <div class = "mt-5 d-flex justify-content-center gap-2">
-                <input type="submit" class ="btn btn-primary" name = "submit" value="creat">
-                <h6>already have one? <a href="/creataccount">log in</a></h6>
-              </div>
-              
-            </form>
-          </div>
+<div class="bg-gray-100 flex justify-center items-center h-screen">
+        <!-- Left: Image -->
+        <div class="w-1/2 h-screen lg:block hidden">
+            <img src="/create.png" alt="Placeholder Image" class="object-cover w-full h-full">
         </div>
-    </div>    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <div class="lg:p-20 md:p-52 sm:p-20 p-8 w-full lg:w-1/2">
+@if ($creatcheck)
+       <div id="alert" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Erreur!</strong>
+                <span class="block sm:inline">Ce compte est déja existe.</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="fill-current h-6 w-6 text-red-500" onclick="closeAlert()" role="button" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+                    </svg>
+                </span>
+        </div>
+@endif
+<h1 class="text-3xl font-semibold mb-4 text-center">Create Account</h1>
+            <form action="/home/me" method="post">
+                @csrf
+                <div class="mb-1">
+                    <label for="firstname" class="block text-gray-600">First Name</label>
+                    <input placeholder="Enter Your First Name" type="text" id="nom" name="firstname" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off" required>
+                </div>
+                <div class="mb-1">
+                    <label for="lastname" class="block text-gray-600">Last Name</label>
+                    <input placeholder="Enter Your Last Name" type="text" id="prenom" name="lastname" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off" required>
+                </div>
+                <!-- Username Input -->
+                <div class="mb-1">
+                    <label for="email" class="block text-gray-600">Email</label>
+                    <input placeholder="Enter Your Email" type="email" id="email" name="email" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off" required>
+                </div>
+                <!-- Password Input -->
+                <div class="mb-6">
+                    <label for="password" class="block text-gray-600">Password</label>
+                    <input placeholder="Enter Your Password" type="password" id="password" name="password" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off" required>
+                </div>
+                <button  name="submit" type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Create Account</button>
+            </form>
+            <!-- Sign up Link -->
+            <div class="mt-2 text-center">
+                <p>Already have an account?</p>
+                <a href="/login" class="text-blue-500 hover:underline">Log in</a>
+            </div>
+        </div>
+    </div>
+
     <script>
-      $(document).ready(function(){
-    $('.toast').toast('show');
-});
+        function closeAlert() {
+            var alert = document.getElementById('alert');
+            alert.style.display = 'none';
+        }
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,54 +3,68 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <title>Login</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body >
-  @if ($logincheck)
-  <div class="toast align-items-center bg-danger-subtle border-0 mx-auto w-100" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="d-flex">
-      <div class="toast-body text-danger">
-       erreur: mot de passe ou email est incorrect
-      </div>
-      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-  </div>
-@endif
-    <div class="card mb-3 w-75 mx-auto border-0 " style="margin-top: 10%;">
-        <div class="row g-0 ">
-          <div class="col-md-4 d-flex justify-content-center">
-            <img src="/5fcddbf4f1ce4bf6a2492d5c3bec559d.png" class="img-fluid rounded-start align-self-center">
-          </div>
-          <div class="col-md-8 bg-light-subtle  shadow-lg rounded-3">
-            <form class="card-body" action="/home/me" method="post">
-              @csrf
-              <h1 class="card-title text-center ">log in</h1>
-              <div class="input-group mb-3 mt-5">
-                <span class="input-group-text" id="basic-addon1">email</span>
-                <input type="text" class="form-control" placeholder="email" aria-label="Username" aria-describedby="basic-addon1" name = "email" required>
-              </div>
-              <div class="input-group mb-3 mt-5">
-                <span class="input-group-text" id="basic-addon1">password</span>
-                <input type="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon1" name = "password" required>
-              </div>
-              <div class = "mt-5 d-flex justify-content-center gap-2">
-                <input type="submit" class ="btn btn-primary" value="log in" name = "submit">
-                <h6>you dont have an account ? <a href="/creataccount">creat one</a></h6>
-              </div>
-              
-            </form>
-          </div>
+<body>
+
+    <div class="bg-gray-100 flex justify-center items-center h-screen">
+        <!-- Left: Image -->
+        <div class="w-1/2 h-screen lg:block hidden">
+            <img src="\susan-q-yin-2JIvboGLeho-unsplash.jpg" alt="Placeholder Image" class="object-cover w-full h-full">
         </div>
-    </div>    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <div class="lg:p-20 md:p-52 sm:p-20 p-8 w-full lg:w-1/2">
+            @if ($logincheck)
+            <div id="alert" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Erreur!</strong>
+                <span class="block sm:inline">mot de passe ou email est incorrect</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="fill-current h-6 w-6 text-red-500" onclick="closeAlert()" role="button" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+                    </svg>
+                </span>
+            </div>
+            @endif
+            <h1 class="text-3xl font-semibold mb-4 text-center">Login</h1>
+            <form action="/home/me" method="post">
+                @csrf
+                <!-- Username Input -->
+                <div class="mb-4">
+                    <label for="email" class="block text-gray-600">Email</label>
+                    <input placeholder="Enter Your Email" type="email" id="email" name="email" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off" required>
+                </div>
+                <!-- Password Input -->
+                <div class="mb-4">
+                    <label for="password" class="block text-gray-600">Password</label>
+                    <input placeholder="Enter Your Password" type="password" id="password" name="password" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off" required>
+                </div>
+                <!-- Remember Me Checkbox -->
+                <div class="mb-4 flex items-center">
+                    <input type="checkbox" id="remember" name="remember" class="text-blue-500">
+                    <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
+                </div>
+                <!-- Forgot Password Link -->
+                <div class="mb-6 text-blue-500">
+                    <a href="#" class="hover:underline">Forgot Password?</a>
+                </div>
+                <!-- Login Button -->
+                <button value='log in' name="submit" type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
+            </form>
+            <!-- Sign up Link -->
+            <div class="mt-6 text-center">
+                <p>Do not have an account?</p>
+                <a href="/creataccount" class="text-blue-500 hover:underline">Create one</a>
+            </div>
+        </div>
+    </div>
+
     <script>
-      $(document).ready(function(){
-    $('.toast').toast('show');
-});
+        function closeAlert() {
+            var alert = document.getElementById('alert');
+            alert.style.display = 'none';
+        }
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
